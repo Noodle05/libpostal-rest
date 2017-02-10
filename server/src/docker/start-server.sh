8874d5a584906ffe340f2687018c6c943d7e59f4
Wei Gao <wgao@salesforce.com>
2017-02-09 22:43:54 -0800

    Optimize docker stop behavior

--- server/src/docker/start-server.sh
+++ server/src/docker/start-server.sh
@@ -1,7 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
-exec \
-    /usr/bin/java -jar -server ${JAVA_GC_OPTS} \
+trap 'kill -TERM $PID' TERM INT
+/usr/bin/java -jar -server ${JAVA_GC_OPTS} \
     ${JAVA_HEAP_OPTS} -DhttpPort=8080 \
     -DtcpPort=8090 ${JAVA_EXT_OPTS} \
-    "${LP_SERVER_HOME}/server.jar"
+    "${LP_SERVER_HOME}/server.jar" &
+PID=$!
+wait $PID
+trap - TERM INT
+wait $PID
+EXIT_STATUS=$?
